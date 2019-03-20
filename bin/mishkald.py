4959c140e809055492a561ab6317b4f804aad78b
chrys <chrys@linux-a11y.org>
2019-03-20 13:31:47 +0100

    disable syntax

--- bin/mishkald.py
+++ bin/mishkald.py
@@ -25,17 +25,15 @@ scriptversion = '0.1'
 AuthorName = "chrys"
 
 TCP_IP = '127.0.0.1'
-TCP_PORT = 6222
+TCP_PORT = 6224
 BUFFER_SIZE = 1024
 
 def getTask(conn):
 #  "Grab command-line arguments"
     options ={ "suggestion" : False, 
-    "disableSyntax" : False,
     "disableSemantic" : False,
     "disableStatistic" : False,
     "strip_tashkeel" : False,
-    "nocache" : False,
     "text" : None,
     }
 
@@ -61,7 +59,6 @@ def start():
         options = getTask(conn)
         text     = options['text']
         strip_tashkeel  = options['strip_tashkeel']
-        disableSyntax   = options['disableSyntax']
         disableSemantic = options['disableSemantic']
         disableStat     = options['disableStatistic']
             
@@ -75,8 +72,6 @@ def start():
         if not strip_tashkeel: 
             if disableSemantic:
                 vocalizer.disable_semantic_analysis()
-            if disableSyntax:
-                vocalizer.disable_syntaxic_analysis()
             if disableStat:
                 vocalizer.disable_stat_tashkeel()
 
