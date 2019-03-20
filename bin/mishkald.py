033ce22bb0570abbb7e14cb34fbfca11c5f9da8d
chrys <chrys@linux-a11y.org>
2019-03-20 13:33:44 +0100

    remove disable stat

--- bin/mishkald.py
+++ bin/mishkald.py
@@ -25,14 +25,12 @@ scriptversion = '0.1'
 AuthorName = "chrys"
 
 TCP_IP = '127.0.0.1'
-TCP_PORT = 6224
+TCP_PORT = 6225
 BUFFER_SIZE = 1024
 
 def getTask(conn):
 #  "Grab command-line arguments"
-    options ={ "suggestion" : False, 
-    "disableSemantic" : False,
-    "disableStatistic" : False,
+    options ={ "disableSemantic" : False,
     "strip_tashkeel" : False,
     "text" : None,
     }
@@ -60,7 +58,6 @@ def start():
         text     = options['text']
         strip_tashkeel  = options['strip_tashkeel']
         disableSemantic = options['disableSemantic']
-        disableStat     = options['disableStatistic']
             
         if not text:
             continue
@@ -72,8 +69,6 @@ def start():
         if not strip_tashkeel: 
             if disableSemantic:
                 vocalizer.disable_semantic_analysis()
-            if disableStat:
-                vocalizer.disable_stat_tashkeel()
 
         if len(lines)>0:
             line = lines[0]
