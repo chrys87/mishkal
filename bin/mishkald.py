80591bfaf44130dbd412e29a350050eacb1bc099
chrys <chrys@linux-a11y.org>
2019-03-20 13:58:32 +0100

    cleanup and error catching

--- bin/mishkald.py
+++ bin/mishkald.py
@@ -1,12 +1,9 @@
 #! /usr/bin/python2
 # -*- coding: UTF-8 -*-
 
-import socket
+import socket, os, sys
 import os
 import sys
-import os.path
-import re
-from glob import glob
 base_dir = os.path.dirname(os.path.realpath(__file__))
 #~ sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), '../support/'))
 #~ sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), '../mishkal'))
@@ -21,32 +18,34 @@ import pyarabic.araby as araby
 
 scriptname = os.path.splitext(base_dir)[0]
 
-scriptversion = '0.1'
+scriptversion = '0.2'
 AuthorName = "chrys"
 
 TCP_IP = '127.0.0.1'
-TCP_PORT = 6227
+TCP_PORT = 6123
 BUFFER_SIZE = 1024
+DEBUG = False
 
 def getTask(conn):
-#  "Grab command-line arguments"
     options ={
         "text" : None,
     }
-
-    data = conn.recv(BUFFER_SIZE)
-    options["text"] = data.decode('utf8')
-
-    # get the information
+    try:
+        data = conn.recv(BUFFER_SIZE)
+        options["text"] = data.decode('utf8')
+    except:
+        conn.close()
+        return (options)
+        
     if not data:
         return (options)
 
-    #if text: print text.encode('utf8')
     return (options)
 
 
 def start():
     acceptSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    acceptSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     acceptSock.bind((TCP_IP, TCP_PORT))
     acceptSock.listen(1)
     vocalizer = ArabicVocalizer.TashkeelClass()
@@ -61,31 +60,25 @@ def start():
 
         lines = text.split('\n')
 
-        counter = 1
+        result = u''
+        for line in lines:
+            line = line.strip()
+            if line == '':
+                continue
+            if line.startswith('#'):
+                continue
 
-        if len(lines)>0:
-            line = lines[0]
-
-
-        while line:
-            if not line.startswith('#'):
-                line = line.strip()
-
-                result = vocalizer.tashkeel(line)                    
-
-                # print result.encode('utf8')
-                counter += 1
-             
-                #~ print result.strip('\n').encode('utf8'),
+            lineResult = vocalizer.tashkeel(line)                    
+            result += ' ' + lineResult
+            
+            if DEBUG:
                 if text:
-                    print result.strip('\n').encode('utf8')
-
-            if counter<len(lines):
-                line = lines[counter]
-            else:
-                line = None
-        conn.send(result.encode('utf-8'))
-        print result.strip('\n').encode('utf8')
-        conn.close()
+                    print lineResult.strip('\n').encode('utf8')
+        try:
+            conn.send(result.encode('utf-8'))
+            if DEBUG:
+                print result.strip('\n').encode('utf8')
+        finally:
+            conn.close()
 if __name__ == '__main__':
     start()
