f31a02c991736102b45afa7a11b06bf1e754c728
chrys <chrys@linux-a11y.org>
2019-08-28 16:45:21 +0200

    Update mishkald.py

--- bin/mishkald.py
+++ bin/mishkald.py
@@ -20,103 +20,114 @@ scriptname = os.path.splitext(base_dir)[0]
 scriptversion = '0.3'
 AuthorName = "chrys"
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 6123
-BUFFER_SIZE = 65536
-DEBUG = False
 
-sockets = []
 
-def addSocket(conn):
-    sockets.append(conn)
+class mishkald:
+    def __init__(self):
+        self.ip = '127.0.0.1'
+        self.running = True       
+        self.port = 6123
+        self.buffer = 65536
+        self.debug = False        
+        self.sockets = []
+        self.acceptSock = None
+        self.vocalizer = None
+    def addSocket(self, conn):
+        self.sockets.append(conn)
+    def closeSock(self, conn):
+        try:
+            conn.close()
+        except:
+            pass
+        try:
+            self.sockets.remove(conn)
+        except:
+            pass
+    def isSocketsEmpty(self):
+        return self.sockets == []
+    def getData(self, conn):
+        options ={
+            "text" : None,
+        }
+        try:
+            data = conn.recv(self.buffer)
+            while  '\00' not in data:
+                data += conn.recv(self.buffer)
+            options["text"] = data.decode('utf8').replace('\00', '')
+        except:
+            closeSock(conn)
+            return (options)
 
-def closeSock(conn):
-    try:
-        conn.close()
-    except:
-        pass
-    try:
-        sockets.remove(conn)
-    except:
-        pass
+        if not data:
+            return (options)
 
-def getData(conn):
-    options ={
-        "text" : None,
-    }
-    try:
-        data = conn.recv(BUFFER_SIZE)
-        while  '\00' not in data:
-            data += conn.recv(BUFFER_SIZE)
-        options["text"] = data.decode('utf8').replace('\00', '')
-    except:
-        closeSock(conn)
         return (options)
-        
-    if not data:
-        return (options)
+    def isDebug(self):
+        return self.debug
+    def run(self):
+        self.acceptSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.acceptSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.acceptSock.bind((self.ip, self.port))
+        self.acceptSock.listen(1)
+        self.addSocket(self.acceptSock)
+        self.vocalizer = ArabicVocalizer.TashkeelClass('/tmp/mishkal_cache/')
+        self.vocalizer.set_log_level(50) # critical
+        while self.running:
+            ready = select.select(sockets, [], [], -1)
+            # only accept connection and skip
+            if self.acceptSock in ready:        
+                conn, addr = self.acceptSock.accept()
+                self.acceptSock(conn)
+                ready.remove(self.acceptSock)
+            # for better reading skip if there are no more requests
+            if ready == []:
+                continue
+            # handle outstanding requests
+            for conn in ready:
+                options = self.getData(conn)
+                text = options['text']
 
-    return (options)
+                if not text:
+                    continue
+
+                lines = text.split('\n')
+
+                result = u''
+                for line in lines:
+                    line = line.strip()
+                    if line == '':
+                        continue
+                    if line.startswith('#'):
+                        continue
+
+                    lineResult = self.vocalizer.tashkeel(line)                    
+                    result += ' ' + lineResult
+
+                    if self.isDebug():
+                        if text:
+                            print lineResult.strip('\n').encode('utf8')
+                try:
+                    answer = result + '\00'
+                    conn.send(answer.encode('utf-8'))
+                    if self.isDebug():
+                        print result.strip('\n').encode('utf8')
+                finally:
+                    self.closeSock(conn)
+                if self.isSocketsEmpty():
+                    return
 
 def main():
-    acceptSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    acceptSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    acceptSock.bind((TCP_IP, TCP_PORT))
-    acceptSock.listen(1)
-    addSocket(acceptSock)
-    vocalizer = ArabicVocalizer.TashkeelClass('/tmp/mishkal_cache/')
-    vocalizer.set_log_level(50) # critical
-    running = True
-    while running:
-        ready = select.select(sockets, [], [], -1)
-        # only accept connection and skip
-        if acceptSock in ready:        
-            conn, addr = acceptSock.accept()
-            acceptSock(conn)
-            ready.remove(acceptSock)
-        # for better reading skip if there are no more requests
-        if ready == []:
-            continue
-        # handle outstanding requests
-        for conn in ready:
-            options = getData(conn)
-            text = options['text']
+    app = mishkald()
+    app.proceed()
+    del app
 
-            if not text:
-                continue
-
-            lines = text.split('\n')
-
-            result = u''
-            for line in lines:
-                line = line.strip()
-                if line == '':
-                    continue
-                if line.startswith('#'):
-                    continue
-
-                lineResult = vocalizer.tashkeel(line)                    
-                result += ' ' + lineResult
-
-                if DEBUG:
-                    if text:
-                        print lineResult.strip('\n').encode('utf8')
-            try:
-                answer = result + '\00'
-                conn.send(answer.encode('utf-8'))
-                if DEBUG:
-                    print result.strip('\n').encode('utf8')
-            finally:
-                closeSock(conn)
-            if sockets == []:
-                return
 if __name__ == '__main__':
     try:
         print 'start as daemon'
         pid = "/run/mishkal.pid"
         from daemonize import Daemonize
         daemon = Daemonize(app="mishkald", pid=pid, action=main)
-        daemon.start()
+        daemon.run()
     except Exception as e:
         print 'starting daemon failed, start in foreground'
         print e
